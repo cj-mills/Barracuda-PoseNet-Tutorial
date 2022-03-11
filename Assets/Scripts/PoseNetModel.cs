168b34de0f9c9a231c94401360468977df07860a
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2022-03-10 18:14:27 -0800

    Update PoseNetModel.cs

--- Assets/Scripts/PoseNetModel.cs
+++ Assets/Scripts/PoseNetModel.cs
@@ -11,11 +11,16 @@ public class PoseNetModel : ScriptableObject
     [Tooltip("The ONNX model asset file to use when performing inference")]
     public NNModel modelAsset;
 
+    [Tooltip("The material with the required preprocessing shader")]
     public Material preprocessingMaterial;
 
+    [Tooltip("The index for the heatmap output layer")]
     public int heatmapLayerIndex = 0;
+    [Tooltip("The index for the offsets output layer")]
     public int offsetsLayerIndex = 1;
+    [Tooltip("The index for the forward displacement layer")]
     public int displacementFWDLayerIndex;
+    [Tooltip("The index for the backwared displacement layer")]
     public int displacementBWDLayerIndex;
 
     public void OnEnable()
